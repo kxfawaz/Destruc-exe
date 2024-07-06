29438f480fe7b99c77a88d45e0c4fbebccccd114
kxfawaz <166047017+kxfawaz@users.noreply.github.com>
2024-07-06 13:10:31 -0400

    Add files via upload

---
+++ Destructuring Exe/app.js
@@ -0,0 +1,58 @@
+// //Question 1:
+
+// // 8
+// // 1846
+
+// //Question 2:
+
+// //Returns { yearNeptuneDiscovered: 1846
+//   yearMarsDiscovered: 1659}
+
+
+//Question 3:
+
+// Your name is Alejandro and you like purple
+// Your name is Melissa and you like green
+// Your name is undefined and  you like green
+
+
+//Question 4:
+//"Maya"
+//"Marisa"
+// "Chi"
+
+
+// Question 5 
+// "Raindrops on roses",
+// "whiskers on kittens"
+
+// ["Bright copper kettles","warm woolen mittens","Brown paper packages tied up with strings"]
+
+
+
+//   Question 6
+ 
+
+// [10,30,20]
+
+
+
+//Question 7
+
+const obj = {
+    numbers: {
+        a:1,
+        b:2
+    }
+};
+
+const{numbers:{a,b}} = obj
+
+
+//Question 8 
+[arr[0],arr[1]] = [arr[1],arr[0]]
+
+//Question 9
+
+const raceResults = ([first,second,third,...rest]) => ({first,second,third,rest})
+
